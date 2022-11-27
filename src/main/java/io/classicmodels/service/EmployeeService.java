4d935a83cb3e7525226e59d24adaf211d0d3110a
simonwandera <simonwandera12@gmail.com>
2022-11-27 23:04:21 +0300

    Employee service implementation

--- src/main/java/io/classicmodels/service/EmployeeService.java
+++ src/main/java/io/classicmodels/service/EmployeeService.java
@@ -1,4 +1,18 @@
 package io.classicmodels.service;
 
+import io.classicmodels.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    Employee createEmployee(Employee employee);
+
+    Employee getEmployee(Integer employeeNumber);
+
+    Employee updateEmployee(Employee employee);
+
+    boolean deleteEmployee(Integer employeeNumber);
 }
--- src/main/java/io/classicmodels/service/impl/EmployeeServiceImpl.java
+++ src/main/java/io/classicmodels/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,38 @@
 package io.classicmodels.service.impl;
 
+import io.classicmodels.DAO.EmployeeDAO;
+import io.classicmodels.entity.Employee;
 import io.classicmodels.service.EmployeeService;
 
+import javax.inject.Inject;
+import java.util.List;
+
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Inject
+    private EmployeeDAO employeeDAO;
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.listAll();
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeDAO.insert(employee);
+    }
+
+    @Override
+    public Employee getEmployee(Integer employeeNumber) {
+        return employeeDAO.read(employeeNumber);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return employeeDAO.insert(employee);
+    }
+
+    @Override
+    public boolean deleteEmployee(Integer employeeNumber) {
+        return employeeDAO.deleteById(employeeNumber);
+    }
 }
