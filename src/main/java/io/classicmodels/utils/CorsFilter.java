3feee255cf605b3177fd3db353722f03e7d2e9e3
simonwandera <simonwandera12@gmail.com>
2022-12-06 01:14:03 +0300

    Added  x-requested-with on cors filter

--- src/main/java/io/classicmodels/utils/CorsFilter.java
+++ src/main/java/io/classicmodels/utils/CorsFilter.java
@@ -16,8 +16,10 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        System.out.println("\n\n************** WE ARE THE ONES *************\n\n");
     }
 
 }
